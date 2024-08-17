8a6b08d864f7828be981e8484ae29d2940d3a5b0
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-17 19:08:33 -0300

    feature: created pessoa1 from class Person

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploFundamentos.Models;
+
+Person pessoa1 = new Person();
+
+pessoa1.Name = "Izzy";
+pessoa1.Age = 27;
+pessoa1.Presentation();
