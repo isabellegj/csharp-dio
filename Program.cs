70a89ff8f950743394f3b55643da45c5d3db007e
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-17 20:40:38 -0300

    feature: data types and variables

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
 ﻿using ExemploFundamentos.Models;
 
-Person person1 = new Person();
+string presentation = "Oláaa, tudo beimmm?";
+int quantity = 1;
+double size = 1.60;
+decimal price = 1.75M;
+bool trueOrFalse = true;
 
-person1.Name = "Izzy";
-person1.Age = 27;
-person1.Presentation();
+Console.WriteLine(presentation);
+Console.WriteLine("Valor da variável quantity: " + quantity);
+Console.WriteLine("Valor da variável size: " + size);
+Console.WriteLine("Valor da variável price: " + price);
+Console.WriteLine("Valor da variável trueOrFalse: " + trueOrFalse);
+
+// Person person1 = new Person();
+
+// person1.Name = "Izzy";
+// person1.Age = 27;
+// person1.Presentation();
