30df3057fe28cf5def1bf5486eed244f3ea6a45e
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-17 18:48:11 -0300

    feature: created class Pessoa

---
+++ Models/Pessoa.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.Models
+{
+    public class Pessoa
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public void Presentation()
+        {
+            Console.WriteLine($"Hello, my name is {Name}, I'm {Age} years old.");
+        }
+    }
+}
