a49f7e7c030c163cd78cff129c8b58593401f4ad
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-19 19:47:43 -0300

    conditional operators and debugging

--- Program.cs
+++ Program.cs
@@ -1,18 +1,35 @@
 ﻿using ExemploFundamentos.Models;
 
+// Operadores Condicionais
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+if(possivelVenda)
+{
+    Console.WriteLine("Venda realizada.");
+}
+else {
+    Console.WriteLine("Desculpe, não temos a quantidade em estoque.");
+}
+
 // Cast - Casting
-int a = Convert.ToInt32(null); // O Convert "trata" o null como 0, evita erros
-int b = int.Parse("5");
-string c = 5.ToString();
+// int a = Convert.ToInt32(null); // O Convert "trata" o null como 0, evita erros
+// int b = int.Parse("5");
+// string c = 5.ToString();
 
-// Casting implícito
-int d = 5;
-double e = d;
+// // Casting implícito
+// int d = 5;
+// double e = d;
 
-// Casting seguro
-string tal = "15-";
-int tal2 = 0;
-int.TryParse(tal, out tal2); // vai sair o valor de tal2, pois o parse não deu certo e ele manteve o valor original de tal2
+// // Casting seguro
+// string tal = "15-";
+// int tal2 = 0;
+// int.TryParse(tal, out tal2); // vai sair o valor de tal2, pois o parse não deu certo e ele manteve o valor original de tal2
 
 // DateTime dataAtual = DateTime.Now.AddDays(5);
 // Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
