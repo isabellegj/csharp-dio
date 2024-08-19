a9abb426560542837669150f0ce3985cd131a673
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-19 19:52:18 -0300

    nested if

--- Program.cs
+++ Program.cs
@@ -3,13 +3,16 @@
 // Operadores Condicionais
 int quantidadeEmEstoque = 10;
 int quantidadeCompra = 4;
-bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
 
 Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
 Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
 Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
-if(possivelVenda)
+if(quantidadeCompra == 0) {
+    Console.WriteLine("Venda inválida");
+}
+else if(possivelVenda)
 {
     Console.WriteLine("Venda realizada.");
 }
