e99f2a45488223da2aa0f704307c52d7da92be03
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-19 20:02:32 -0300

    switch case

--- Program.cs
+++ Program.cs
@@ -1,24 +1,42 @@
 ﻿using ExemploFundamentos.Models;
 
 // Operadores Condicionais
-int quantidadeEmEstoque = 10;
-int quantidadeCompra = 4;
-bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+Console.WriteLine("Digite uma letra: ");
+string letra = Console.ReadLine();
 
-Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
-Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
-Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
-
-if(quantidadeCompra == 0) {
-    Console.WriteLine("Venda inválida");
-}
-else if(possivelVenda)
+switch(letra)
 {
-    Console.WriteLine("Venda realizada.");
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+    Console.WriteLine("Vogal");
+    break;
+
+    default:
+    Console.WriteLine("Consoante");
+    break;
 }
-else {
-    Console.WriteLine("Desculpe, não temos a quantidade em estoque.");
-}
+
+// int quantidadeEmEstoque = 10;
+// int quantidadeCompra = 4;
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+// Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+// if(quantidadeCompra == 0) {
+//     Console.WriteLine("Venda inválida");
+// }
+// else if(possivelVenda)
+// {
+//     Console.WriteLine("Venda realizada.");
+// }
+// else {
+//     Console.WriteLine("Desculpe, não temos a quantidade em estoque.");
+// }
 
 // Cast - Casting
 // int a = Convert.ToInt32(null); // O Convert "trata" o null como 0, evita erros
