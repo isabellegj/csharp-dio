45c051362a224898f0fffb8fef36be2d1c208eed
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-19 21:13:54 -0300

    feature: added Potencia, Seno, Coseno and Tangente methods to Calculadora Class

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -23,5 +23,31 @@ namespace ExemploFundamentos.Models
         {
             Console.WriteLine($"{x} / {y} = {x / y}");
         }
+         public void Potencia(int x, int y)
+        {
+            double potencia = Math.Pow(x, y);
+            Console.WriteLine($"{x} ^ {y} = {potencia}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {seno}");
+        }
+
+             public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {coseno}");
+        }
+
+             public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {tangente}");
+        }
     }
 }
