0b40880140f4e6f9476823c3bb67d2db2fe8110d
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-20 21:13:08 -0300

    Loops

--- Program.cs
+++ Program.cs
@@ -1,8 +1,142 @@
 ﻿using ExemploFundamentos.Models;
 
-Calculadora calc = new Calculadora();
+string opcao;
+bool exibirMenu = true;
 
-calc.Somar(4, 8);
+while(exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch(opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de Cliente");
+            break;
+         case "2":
+            Console.WriteLine("Busca de Cliente");
+            break;
+         case "3":
+            Console.WriteLine("Apagar Cliente");
+            break;
+         case "4":
+            Console.WriteLine("Encerrar");
+            // Environment.Exit() para sair AQUI, não executa mais nada abaixo
+            exibirMenu = false;
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int soma = 0;
+// int numero = 0;
+
+// do
+// {
+//     Console.WriteLine("Digite um número (0 para parar): ");
+//     numero = Convert.ToInt32(Console.ReadLine());
+
+//     soma += numero;
+
+// } while (numero != 0);
+
+// Console.WriteLine($"Total da soma dos números digitados é: {soma}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int numero = 5;
+// int contador = 0;
+
+// while (contador <= 10)
+// {
+//     Console.WriteLine($"{numero} X {contador} = {numero * contador}");
+//     contador++;
+
+//     if (contador ==6)
+//     {
+//         break;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int numero = 5;
+
+// for(int contador = 0; contador <= 10; contador++)
+// {
+//     Console.WriteLine($"{numero} X {contador} = {numero * contador}");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Calculadora calc = new Calculadora();
+
+// calc.Somar(4, 8);
 
 
 
